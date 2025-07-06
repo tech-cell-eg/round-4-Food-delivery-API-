5d4443b2d61883fb716986cb74dc2b9cc1b066e8
atefAkl <atefakl80@gmail.com>
2025-07-06 21:37:54 +0300

    remove unwanted seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,8 +25,6 @@ class DatabaseSeeder extends Seeder
             DishIngredientSeeder::class, // Link dishes with ingredients (depends on dishes and ingredients)
             CouponSeeder::class,        // Create coupons (depends on chefs)
             ReviewSeeder::class,
-            OrderSeeder::class,         
-            OrderItemSeeder::class,     
         ]);
     }
 }
