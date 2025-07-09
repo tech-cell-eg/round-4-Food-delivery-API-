6e363ec724770a1cd4d4492dc4edcbd5af4d89bd
MohamedAhmedDev <160210223+MohamedAhmed479@users.noreply.github.com>
2025-07-09 15:51:05 +0300

    Update Customer.php

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -22,7 +22,7 @@ class Customer extends Model
      */
     public function user()
     {
-        return $this->belongsTo(User::class, 'id');
+        return $this->belongsTo(User::class, 'id', 'id');
     }
 
     /**
