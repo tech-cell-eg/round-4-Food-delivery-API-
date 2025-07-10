b64198202d02d401dbc6b41dd079dabf874e10ae
MohamedAhmedDev <160210223+MohamedAhmed479@users.noreply.github.com>
2025-07-10 19:26:41 +0300

    Update api.php
    Add NameSpace for StatisticsController

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 
 use Illuminate\Support\Facades\Auth;
 
+use App\Http\Controllers\API\Chef\StatisticsController;
 // ==================== Auth ====================
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
