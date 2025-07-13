668e16bc2921e9ea0ae272ec02f90cb4d91cedb3
MohamedAhmedDev <160210223+MohamedAhmed479@users.noreply.github.com>
2025-07-13 19:30:46 +0300

    Update DatabaseSeeder.php
    Add Conversation & Messages Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -27,6 +27,8 @@ class DatabaseSeeder extends Seeder
             CouponSeeder::class,        // Create coupons (depends on chefs)
             AdminSeeder::class,
             // ReviewSeeder::class,
+            MessagesTableSeeder::class,
+            ConversationsTableSeeder::class,
         ]);
     }
 }
