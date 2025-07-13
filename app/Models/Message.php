1ad2e5b56f24e9f86549f1c850ca28a2d0a2bdb8
MohamedAhmedDev <160210223+MohamedAhmed479@users.noreply.github.com>
2025-07-13 19:32:21 +0300

    Update Message.php

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Message extends Model
 {
+    use HasFactory;
+    
     protected $fillable = [
         "conversation_id",
         "sender_id",
